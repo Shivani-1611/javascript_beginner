896321ad8b36bb3bb7fa9057c7a924ad831d9556
shivani-16 <vibhuanant@gmail.com>
2022-05-14 22:30:43 +0530

    return call in function

---
+++ function-2.js
@@ -0,0 +1,12 @@
+function monitorCount(rows,columns){
+    return rows*columns;
+  }
+  const numOfMonitors= monitorCount(5,4);
+  console.log(numOfMonitors);
+  // rows=5;
+  // columns=4;
+  function rectangleArea(width,height){
+    let area= width*height;
+    return area;
+  }
+  console.log(rectangleArea(5,7))
