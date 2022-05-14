276826226440684e1c18ce75d151a9c524f696b1
shivani-16 <vibhuanant@gmail.com>
2022-05-14 23:16:56 +0530

    arrow function

---
+++ arrow-function-1.js
@@ -0,0 +1,11 @@
+const rectangleArea=(width,height)=>{
+    let area= width*height;
+    return area;
+  }
+  const plantNeedsWater = (day)=> {
+    if (day === 'Wednesday') {
+      return true;
+    } else {
+      return false;
+    }
+  };
