439664054afa81a898ab4f95602a8a77e2bffdda
shivani-16 <vibhuanant@gmail.com>
2022-05-15 00:42:23 +0530

    global scope

---
+++ global-scope-1.js
@@ -0,0 +1,15 @@
+const satellite= 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+const callMyNightSky = () => {
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and '+ galaxy;
+};
+console.log(callMyNightSky());
+
+const color= 'blue';
+
+const returnSkyColor = () =>{
+  return color;
+};
+console.log(returnSkyColor());
