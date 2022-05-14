75b4d1fb6f0c424c68fb7877b4cded6e6e2cd138
shivani-16 <vibhuanant@gmail.com>
2022-05-14 19:26:55 +0530

    functios practice-1

---
+++ function-1.js
@@ -0,0 +1,6 @@
+function sayThanks(name) {
+    console.log(`Thank you ${name} for your purchase! We appreciate your business.`);
+    console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+  }
+  let name="Cole";
+  sayThanks(name);
