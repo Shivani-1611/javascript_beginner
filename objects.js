7c46ae7d98527cb9520f7c8debc784afc1b147c0
Shivani-1611 <100325338+Shivani-1611@users.noreply.github.com>
2022-05-21 17:39:45 +0530

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,13 @@
+const cars ={type: "Fiat", model: "500", color:"white"};
+cars.color;
+
+cars.owner = "Shivani";
+console.log(cars.owner);
+
+const x = 5;
+console.log(x);
+
+{
+    const x = 2;
+    console.log(x);
+}
